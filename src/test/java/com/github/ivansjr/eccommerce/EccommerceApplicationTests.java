ae694e16e4d088c9914a937de461c42a80402940
Ivan Júnior <ivan.junior2706@gmail.com>
2022-07-17 22:00:24 -0300

    Created project and configure entities.

---
+++ src/test/java/com/github/ivansjr/eccommerce/EccommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ivansjr.eccommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EccommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
